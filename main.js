446c409917308961115ce6906c6a712ede985266
Marina <marinavalentinruiz@gmail.com>
2019-07-31 16:26:13 +0200

    grafica centrada

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
 var chart = c3.generate({
 
-      bindto: "#chart",
-      data: {
-      //   url:'Prueba.csv',
-      //   type: 'spline'
-      // },
-        columns: [
-          ["Madrid", 200, 50, 80, 100],
-          ["Barcelona", 50, 600, 532, 50],
-          ["Valencia", 400, 80, 56, 300]
-        ]
-      }
-        //type: "spline"
-      //},
-      // grid: {
-      //   x: {
-      //     show: true
-      //   },
-      //   y: {
-      //     show: true
-      //   }
-      // }
-      });
+  bindto: "#chart",
+  // size: {
+  //   height: 400,
+  //   width: 800,
+  // },
+  //
+  // padding: {
+  //   top:150,
+  //   left: 300,
+  // },
 
-      // setTimeout(function(){
-      //   c3.generate({
-      //     data:{
-      //       url:'Prueba.csv',
-      //       mimeType: 'csv'
-      //     }
-      //   });
-      // }, 2000);
-      // setTimeout(function(){
-      //   chart.unload({
-      //     ids: 'Madrid'
-      //   });
-      // }, 3000);
+  data: {
+    //   url:'Prueba.csv',
+    //   type: 'spline'
+    // },
+    columns: [
+      ["Madrid", 200, 50, 80, 100],
+      ["Barcelona", 50, 600, 532, 50],
+      ["Valencia", 400, 80, 56, 300]
+    ]
+  }
+  //type: "spline"
+  //},
+  // grid: {
+  //   x: {
+  //     show: true
+  //   },
+  //   y: {
+  //     show: true
+  //   }
+  // }
+});
+
+// setTimeout(function(){
+//   c3.generate({
+//     data:{
+//       url:'Prueba.csv',
+//       mimeType: 'csv'
+//     }
+//   });
+// }, 2000);
+// setTimeout(function(){
+//   chart.unload({
+//     ids: 'Madrid'
+//   });
+// }, 3000);
