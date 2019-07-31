311e531973bc158d3d0d2cff65bcab3e6a8dcf8a
Marina <marinavalentinruiz@gmail.com>
2019-07-31 09:41:55 +0200

    primera carga

---
+++ main.js
@@ -0,0 +1,38 @@
+var chart = c3.generate({
+
+      bindto: "#chart",
+      data: {
+      //   url:'Prueba.csv',
+      //   type: 'spline'
+      // },
+        columns: [
+          ["Madrid", 200, 50, 80, 100],
+          ["Barcelona", 50, 600, 532, 50],
+          ["Valencia", 400, 80, 56, 300]
+        ]
+      }
+        //type: "spline"
+      //},
+      // grid: {
+      //   x: {
+      //     show: true
+      //   },
+      //   y: {
+      //     show: true
+      //   }
+      // }
+      });
+
+      // setTimeout(function(){
+      //   c3.generate({
+      //     data:{
+      //       url:'Prueba.csv',
+      //       mimeType: 'csv'
+      //     }
+      //   });
+      // }, 2000);
+      // setTimeout(function(){
+      //   chart.unload({
+      //     ids: 'Madrid'
+      //   });
+      // }, 3000);
